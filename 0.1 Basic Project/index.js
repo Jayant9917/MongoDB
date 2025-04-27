e9d1eede3b09140315eddb937d26e7b0cb3761f2
Jayant9917 <ranajayant527@gmail.com>
2025-04-27 20:50:02 +0530

    adding database

--- 0.1 Basic Project/index.js
+++ 0.1 Basic Project/index.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const JWT_SECRET = "s3cret";
+mongoose.connect("mongodb+srv://gasfgfafa:Aa5jxKhylWdFhv4v@cluster0.7kmvq.mongodb.net/todo-app")
+
+const { auth, JWT_SECRET } = require("./auth");
+const { UserModel, TodoModel } = require("./db");
+
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.post("/signup", function(req, res) {
+app.post("/signup", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    });
     
+    res.json({
+        message: "You are signed up"
+    })
 });
 
 
-app.post("/signin", function(req, res) {
+app.post("/signin", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const response = await UserModel.findOne({
+        email: email,
+        password: password,
+    });
+
+    if (response) {
+        const token = jwt.sign({
+            id: response._id.toString()
+        })
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
 });
 
 

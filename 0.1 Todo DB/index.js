44845a2d25692456542e4958a1b724c2435cfe0d
Jayant9917 <ranajayant527@gmail.com>
2025-05-04 04:37:12 +0530

    bcrypt update

--- 0.1 Todo DB/index.js
+++ 0.1 Todo DB/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 
-
-mongoose.connect("mongodb+srv://admin:jayant132465rana@cluster0.cty7ree.mongodb.net/Todo-app-database");
+mongoose.connect("mongodb+srv://admin:jayant132465rana@cluster0.cty7ree.mongodb.net/Todo-app-database")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
 
 const app = express();
@@ -13,34 +15,41 @@ const port = 3000;
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
+    try {
+        const { email, password, name } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
-    
-    
-    res.json({
-        message: "You are signed up"
-    })
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+
+        res.json({
+            message: "You are signed up"
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong during signup" });
+    }
 });
 
 
+
 app.post("/signin", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+    try{
+        const { email, password } = req.body;
 
     const response = await UserModel.findOne({
         email: email,
-        password: password,
     });
-    console.log(response);
-
-    if (response) {
+    if (!response){
+        res.status(403).json({
+            "msg" : "user does not exist in db"
+        })
+        return
+    }
+    const passwordMatch = await bcrypt.compare(password, response.password);
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
         }, JWT_SECRET)
@@ -53,14 +62,15 @@ app.post("/signin", async function(req, res) {
             message: "Incorrect creds"
         })
     }
-    
+}catch(error){
+    res.status(500).json({ error: "Something went wrong" });
+    }
 });
 
 
-app.post("/todo", async function(req, res) {
+app.post("/todo", auth, async function(req, res) {
     const userId = req.userId;
-    const title = req.body.title;
-    const done = req.body.done;
+    const { title, done } = req.body;
 
     await TodoModel.create({
         userId,
@@ -74,7 +84,7 @@ app.post("/todo", async function(req, res) {
 });
 
 
-app.get("/todos", async function(req, res) {
+app.get("/todos", auth, async function(req, res) {
     const userId = req.userId;
 
     const todos = await TodoModel.find({
